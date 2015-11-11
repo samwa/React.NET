fa808768a5c26b4d270c6e5072da858e88e6e6f5
samwa <sam.waterhouse@reactive.com>
2015-11-11 11:33:45 +1100

    changing refs for author

--- src/React.Sample.Mvc4/Content/Sample.jsx
+++ src/React.Sample.Mvc4/Content/Sample.jsx
@@ -109,8 +109,8 @@ var Comment = React.createClass({
 var CommentForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
-    var authorName = this.refs.authorname.getDOMNode().value.trim();
-    var authorGithubusername = this.refs.authorgithubusername.getDOMNode().value.trim();
+    var authorName = this.refs.author_name.getDOMNode().value.trim();
+    var authorGithubusername = this.refs.author_githubusername.getDOMNode().value.trim();
 	var author = { Name: authorName, GithubUsername: authorGithubusername };
     var text = this.refs.text.getDOMNode().value.trim();
 
@@ -118,16 +118,16 @@ var CommentForm = React.createClass({
       return;
     }
     this.props.onCommentSubmit({Author: author, Text: text});
-    this.refs.authorname.getDOMNode().value = '';
-    this.refs.authorgithubusername.getDOMNode().value = '';
+    this.refs.author_name.getDOMNode().value = '';
+    this.refs.author_githubusername.getDOMNode().value = '';
     this.refs.text.getDOMNode().value = '';
     return;
   },
   render() {
     return (
       <form className="commentForm" method="post" action={this.props.action} onSubmit={this.handleSubmit}>
-        <input name="Author.Name" type="text" placeholder="Your name" ref="authorname" />
-        <input name="Author.GithubUsername" type="text" placeholder="Your github username" ref="authorgithubusername" />
+        <input name="Author.Name" type="text" placeholder="Your name" ref="author_name" />
+        <input name="Author.GithubUsername" type="text" placeholder="Your github username" ref="author_githubusername" />
         <input name="Text" type="text" placeholder="Say something..." ref="text" />
         <input type="submit" value="Post" />
       </form>
